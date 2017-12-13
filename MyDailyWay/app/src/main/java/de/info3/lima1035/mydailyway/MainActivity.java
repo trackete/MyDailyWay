92db8899e3e2c70d249378d7826cfca30193fe52
trackete <markus.linnartz@gmail.com>
2017-12-13 18:56:48 +0100

    blabla

--- MyDailyWay/app/src/main/java/de/info3/lima1035/mydailyway/MainActivity.java
+++ MyDailyWay/app/src/main/java/de/info3/lima1035/mydailyway/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    // hallo //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.button_start_tracking);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
