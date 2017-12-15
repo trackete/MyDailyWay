9c40e7336eab976f226d6a7d10ad0a553349390b
DavidPatrickAdam <david76829@gmail.com>
2017-12-15 13:40:06 +0100

    Logo eingefügt der Rest kommt morgen dudes

--- MyDailyWay/app/src/main/java/de/info3/lima1035/mydailyway/MainActivity.java
+++ MyDailyWay/app/src/main/java/de/info3/lima1035/mydailyway/MainActivity.java
@@ -88,7 +88,7 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery)  {
+        } else if (id == R.id.nav_export)  {
 
         } else if (id == R.id.nav_slideshow) {
 
