4426e25b5870093eff76c45a3d5ef70bafb8c322
selinewi <seline.winkelmann@gmail.com>
2017-12-18 22:35:58 +0100

    Versuch Standorterkennung

--- MyDailyWay/app/src/main/java/de/info3/lima1035/mydailyway/MainActivity.java
+++ MyDailyWay/app/src/main/java/de/info3/lima1035/mydailyway/MainActivity.java
@@ -1,10 +1,14 @@
 package de.info3.lima1035.mydailyway;
 
+import android.Manifest;
 import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -40,9 +44,13 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
-        //Seline Winkelmann: GoogleMaps implementiern (Code:AIzaSyCxPveUpfQr6cvTTdwYjbnkRyeieIJsmmY)
+
+        //Seline Winkelmann: GoogleMaps implementieren (Code:AIzaSyCxPveUpfQr6cvTTdwYjbnkRyeieIJsmmY)
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
+
+
+
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
@@ -52,8 +60,72 @@ public class MainActivity extends AppCompatActivity
                 LatLng sydney = new LatLng(-34, 151);
                 GoogleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
                 GoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
             }
         });
+
+        /*
+        //Seline Winkelmann: Abfragen ob App Erlaubnis hat auf den Standort zuzugreifen
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.LOCATION_HARDWARE);
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.LOCATION_HARDWARE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.LOCATION_HARDWARE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.LOCATION_HARDWARE},
+                        permissionCheck);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+        @Override
+        public void onRequestPermissionsResult(int requestCode,   String permissions[]; int[] grantResults) {
+            switch (requestCode) {
+                case MY_PERMISSIONS_REQUEST_LOCATION: {
+                    // If request is cancelled, the result arrays are empty.
+                    if (grantResults.length > 0
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                        // permission was granted, yay! Do the
+                        // contacts-related task you need to do.
+
+                    } else {
+
+                        // permission denied, boo! Disable the
+                        // functionality that depends on this permission.
+                    }
+                    return;
+                }
+
+                // other 'case' lines to check for other
+                // permissions this app might request
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+
+            GoogleMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+        }*/
 
         //Markus Linnartz: Einfügen der FloatingActionsButtons//
         //Markus Linnartz: Jeweils ein Button pro Verkehrsmittel, wobei nur der Button mit dem Symbol des aktuell ausgewählten Verkehrsmittels unten links angezeigt wird)//
