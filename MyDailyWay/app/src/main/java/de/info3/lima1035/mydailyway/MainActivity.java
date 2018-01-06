af3744f2783af6e4c0d5e60a9ef787df3f63a219
trackete <markus.linnartz@gmail.com>
2018-01-06 14:47:41 +0100

    Launcher

--- MyDailyWay/app/src/main/java/de/info3/lima1035/mydailyway/MainActivity.java
+++ MyDailyWay/app/src/main/java/de/info3/lima1035/mydailyway/MainActivity.java
@@ -345,7 +345,9 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -370,6 +372,8 @@ public class MainActivity extends AppCompatActivity
     private void stopLocationUpdates() {
         mFusedLocationClient.removeLocationUpdates(mLocationCallback);
     }
+
+
 
     @Override
     public void onBackPressed() {
